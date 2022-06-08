3399662e82f425718c1f0101b026f6a86d6dbc61
João Victor Terzi <jv_ermel@hotmail.com>
2022-06-08 11:58:11 -0300

    Adding named routes to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:clothes_store/pages/product_detail_page.dart';
+import 'package:clothes_store/utils/app_routes.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/products_overview_page.dart';
@@ -12,18 +14,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: ProductOverviewPage(),
-        theme: ThemeData().copyWith(
-          colorScheme: ThemeData().colorScheme.copyWith(
-                primary: Colors.purple,
-                secondary: Colors.orange,
-              ),
-          textTheme: const TextTheme().copyWith(
-            headline6: const TextStyle(fontFamily: 'Lato'),
-            subtitle1: const TextStyle(fontFamily: 'Lato'),
-          )
+      debugShowCheckedModeBanner: false,
+      home: ProductOverviewPage(),
+      theme: ThemeData().copyWith(
+        colorScheme: ThemeData().colorScheme.copyWith(
+          primary: Colors.purple,
+          secondary: Colors.orange,
         ),
+        textTheme: const TextTheme().copyWith(
+          headline6: const TextStyle(fontFamily: 'Lato'),
+          subtitle1: const TextStyle(fontFamily: 'Lato', fontSize: 10),
+        ),
+      ),
+      routes: {
+        AppRoutes.PRODUCT_DETAIL: (ctx) => const ProductDetailPage()
+      },
     );
   }
 }
