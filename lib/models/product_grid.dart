06e53ba5af10ad6f19d8c3dc7e5e2d7ed60425f9
João Victor Terzi <jv_ermel@hotmail.com>
2022-06-09 09:12:19 -0300

    Creating the Product Grid Widget and refactoring the code

---
+++ lib/models/product_grid.dart
@@ -0,0 +1,28 @@
+import 'package:clothes_store/data/dummy_data.dart';
+import 'package:clothes_store/models/product.dart';
+import 'package:clothes_store/models/product_list.dart';
+import 'package:clothes_store/widgets/product_item.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(8.0),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+      ),
+      itemBuilder: (ctx, index) => ProductItem(product: dummyProducts[index]),
+      itemCount: loadedProducts.length,
+    );
+  }
+}
+
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,34 +1,18 @@
-import 'package:clothes_store/data/dummy_data.dart';
-import 'package:clothes_store/models/product.dart';
-import 'package:clothes_store/models/product_list.dart';
-import 'package:clothes_store/widgets/product_item.dart';
+import 'package:clothes_store/models/product_grid.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class ProductOverviewPage extends StatelessWidget {
   const ProductOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha loja'),
         centerTitle: true,
         titleTextStyle: Theme.of(context).textTheme.headline6,
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(8.0),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 8,
-          mainAxisSpacing: 8,
-        ),
-        itemBuilder: (ctx, index) => ProductItem(product: dummyProducts[index]),
-        itemCount: loadedProducts.length,
-      ),
-    );
+      body: const ProductGrid(),
+      );
   }
 }
