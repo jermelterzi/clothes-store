9e5618819ca12cb23ff076f534077296009a284b
João Victor Terzi <joao.terzi@afixcode.com.br>
2022-07-08 10:33:18 -0300

    Implementando a conexão com o Firebase através de uma requisição HTTP

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,14 +1,19 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:clothes_store/data/dummy_data.dart';
 import 'package:clothes_store/models/product.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ProductList with ChangeNotifier {
   final List<Product> _items = dummyProducts;
 
+  final _baseURL = 'https://clothes-store-bd94f-default-rtdb.firebaseio.com/';
+
   List<Product> get items => [..._items];
-  List<Product> get favoriteItems => _items.where((prod) => prod.isFavorite).toList();
+  List<Product> get favoriteItems =>
+      _items.where((prod) => prod.isFavorite).toList();
 
   int get itemsCount {
     return _items.length;
@@ -23,7 +28,7 @@ class ProductList with ChangeNotifier {
       price: data['price'] as double,
       imageUrl: data['imageUrl'] as String,
     );
-    if(hasId) {
+    if (hasId) {
       updateProduct(product);
     } else {
       addProduct(product);
@@ -31,13 +36,26 @@ class ProductList with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    http.post(
+      Uri.parse('$_baseURL/products.json'),
+      body: jsonEncode(
+        {
+          'name': product.name,
+          'description': product.description,
+          'price': product.price,
+          'imageURL': product.imageUrl,
+          'isFavorite': product.isFavorite,
+        },
+      ),
+    );
+
     _items.add(product);
     notifyListeners();
   }
 
   void updateProduct(Product product) {
     int index = _items.indexWhere((p) => p.id == product.id);
-    if(index >= 0) {
+    if (index >= 0) {
       _items[index] = product;
       notifyListeners();
     }
@@ -45,7 +63,7 @@ class ProductList with ChangeNotifier {
 
   void removeProduct(Product product) {
     int index = _items.indexWhere((p) => p.id == product.id);
-    if(index >= 0) {
+    if (index >= 0) {
       _items.removeWhere((p) => p.id == product.id);
       notifyListeners();
     }
